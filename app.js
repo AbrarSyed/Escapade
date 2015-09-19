5a32e1da9637e30b51e371ace2fe3e6af9959fc4
Jeffrey Huang <jeffrey.huang977@gmail.com>
2015-09-19 13:25:44 -0700

    Moved stuff around

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var uber = require('./routes/uber');
+var uber = require('./models/uber');
 
 var app = express();
 

3a3e7d9fa718cfbf8e9bab1f5b606cd5b2083ad6
Jeffrey Huang <jeffrey.huang977@gmail.com>
2015-09-19 20:38:15 -0400

    Remove brackets from auto complete

--- routes/flightsIndex.js
+++ routes/flightsIndex.js
@@ -7,6 +7,11 @@ var flights = require('../models/amadeusFlights.js');
 
 router.get('/airportAutoComplete', function(request, response, next) {
     flights.autoComplete(request.query.text).then(function (body) {
+        for (var idx = 0; idx < body.length; idx++)
+        {
+            var temp = body[idx];
+            temp.label = temp.label.substr(0, temp.label.indexOf("[") - 1);
+        }
         response.json(body);
     }, function (reason) {
         response.send(reason);

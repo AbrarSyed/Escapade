3b6cca1eeaaeba3c88a5b90ff38b168e2b2bca19
Jeffrey Huang <jeffrey.huang977@gmail.com>
2015-09-19 20:38:49 -0400

    Fix edge case

--- routes/flightsIndex.js
+++ routes/flightsIndex.js
@@ -10,7 +10,7 @@ router.get('/airportAutoComplete', function(request, response, next) {
         for (var idx = 0; idx < body.length; idx++)
         {
             var temp = body[idx];
-            temp.label = temp.label.substr(0, temp.label.indexOf("[") - 1);
+            temp.label = temp.label.substr(0, temp.label.indexOf("[") - 1 < 0 ? 0 : temp.label.indexOf("[") - 1);
         }
         response.json(body);
     }, function (reason) {

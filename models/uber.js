0cddccdb46f3d03d3db581fc7ae5d061a72dac13
Jeffrey Huang <jeffrey.huang977@gmail.com>
2015-09-19 21:21:09 -0400

    Moved functions into anon functions

--- models/uber.js
+++ models/uber.js
@@ -45,75 +45,6 @@ function callUberAPI(url)
     }
 }
 
-function estimatePrice(startCoord, endCoord)
-{
-    var version = "v1";
-    var type = "estimates/price";
-    var queryString = "start_latitude=" + startCoord['latitude'] + "&" +
-                      "start_longitude=" + startCoord['longitude'] + "&" +
-                      "end_latitude=" + endCoord['latitude'] + "&" +
-                      "end_longitude=" + endCoord['longitude'];
-
-    var url = endpoint + version + "/" + type + "?" + queryString;
-
-    return new Promise(function (resolve, reject) {
-        callUberAPI(url).then(function (data) {
-            var ret = JSON.parse(data);
-            var low = 0.0;
-            var high = 0.0;
-            var num = 0;
-            var filter = "uberx";
-
-            // find the average price of all uberx's
-            for (var idx = 0; idx < ret['prices'].length; idx++) {
-
-                var temp = ret['prices'][idx];
-                if (temp['display_name'].toLowerCase() === filter) {
-                    var surge = temp['surge_multiplier'];
-
-                    low += temp['low_estimate'] * 1/surge;
-                    high += temp['high_estimate'] * 1/surge;
-                    num++;
-                }
-
-                // pass that price back
-                resolve((low + high)/num); // Calculate the average price over all uberX's with no surge and return
-            }
-        }, function (reason) {
-            reject(reason);
-        });
-    });
-}
-
-function estimateTimes(startCoord)
-{
-    var version = "v1";
-    var type = "estimates/time";
-    var queryString = "start_latitude=" + startCoord['latitude'] + "&" +
-        "start_longitude=" + startCoord['longitude'];
-
-    var url = endpoint + version + "/" + type + "?" + queryString;
-
-    return new Promise(function (resolve, reject) {
-        callUberAPI(url).then(function(data) {
-            var ret = JSON.parse(data);
-            var estimate = 0;
-            var filter = "uberx";
-
-            for (var i = 0; i < ret.times.length; i++) {
-                if (ret.times[i].display_name.toLowerCase() === filter) {
-                    estimate = ret.times[i].estimate;
-                    break;
-                }
-            }
-
-            resolve(estimate);
-        }, function(reason) {
-            reject(reason);
-        });
-    })
-}
-
 /**
  * Hits the Uber Time estimate endpoint
  * @param startCoord dict for the longitude and latitude of the place to search
@@ -123,6 +54,69 @@ function estimateTimes(startCoord)
  */
 
 module.exports = {
-    "estimateTimes": estimateTimes,
-    "estimatePrices": estimatePrice
+    "estimateTimes": function(startCoord) {
+        var version = "v1";
+        var type = "estimates/time";
+        var queryString = "start_latitude=" + startCoord['latitude'] + "&" +
+            "start_longitude=" + startCoord['longitude'];
+
+        var url = endpoint + version + "/" + type + "?" + queryString;
+
+        return new Promise(function (resolve, reject) {
+            callUberAPI(url).then(function(data) {
+                var ret = JSON.parse(data);
+                var estimate = 0;
+                var filter = "uberx";
+
+                for (var i = 0; i < ret.times.length; i++) {
+                    if (ret.times[i].display_name.toLowerCase() === filter) {
+                        estimate = ret.times[i].estimate;
+                        break;
+                    }
+                }
+
+                resolve(estimate);
+            }, function(reason) {
+                reject(reason);
+            });
+        })
+    },
+    "estimatePrices": function(startCoord, endCoord) {
+        var version = "v1";
+        var type = "estimates/price";
+        var queryString = "start_latitude=" + startCoord['latitude'] + "&" +
+            "start_longitude=" + startCoord['longitude'] + "&" +
+            "end_latitude=" + endCoord['latitude'] + "&" +
+            "end_longitude=" + endCoord['longitude'];
+
+        var url = endpoint + version + "/" + type + "?" + queryString;
+
+        return new Promise(function (resolve, reject) {
+            callUberAPI(url).then(function (data) {
+                var ret = JSON.parse(data);
+                var low = 0.0;
+                var high = 0.0;
+                var num = 0;
+                var filter = "uberx";
+
+                // find the average price of all uberx's
+                for (var idx = 0; idx < ret['prices'].length; idx++) {
+
+                    var temp = ret['prices'][idx];
+                    if (temp['display_name'].toLowerCase() === filter) {
+                        var surge = temp['surge_multiplier'];
+
+                        low += temp['low_estimate'] * 1/surge;
+                        high += temp['high_estimate'] * 1/surge;
+                        num++;
+                    }
+
+                    // pass that price back
+                    resolve((low + high)/num); // Calculate the average price over all uberX's with no surge and return
+                }
+            }, function (reason) {
+                reject(reason);
+            });
+        });
+    }
 };
